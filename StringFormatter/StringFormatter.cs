074dae371ffb2cf9358b615ce095bef35f90749c
Вероника Богданович <vera022003@gmail.com>
2022-10-18 21:52:20 +0300

    Добавьте файлы проекта.

---
+++ StringFormatter/StringFormatter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace StringFormatter
+{
+    internal class StringFormatter : IStringFormatter
+    {
+        public static readonly StringFormatter Shared = new StringFormatter();
+        public string Format(string template, object target)
+        {
+
+            string[] templateWords = template.Split('{','}',' ');
+            throw new NotImplementedException();
+        }
+    }
+}
